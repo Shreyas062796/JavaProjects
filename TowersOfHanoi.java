1adb16e3c52ddfa4391edefa533059454a94e8a8
Shreyas Ramanujam <Ramanujamshreyas4@gmail.com>
2019-01-05 16:43:51 -0800

    Adding changes for towers of hanoi

--- TowersOfHanoi.java
+++ TowersOfHanoi.java
@@ -5,34 +5,26 @@ import java.util.*;
 public class TowersOfHanoi {
   int x = 1;
     public void Hanoi(int num, String init, String a, String fin){
-        if(num == 1)
-        {
+        if(num == 1){
         System.out.println("Step:"+ x++ +" Disk:"+ num +"  InitialTower:"+ init +"  FinalTower:"+ fin);
         }
-        else
-        {
-
+        else{
         Hanoi(num - 1,init,fin,a);
-
         System.out.println("Step:"+ x++ +" Disk:"+ num +"  InitialTower:"+ init +"  FinalTower:"+ fin);
-
         Hanoi(num - 1,a,init,fin);
         }
         }
 
-public static void main(String[] args)
-{
+public static void main(String[] args){
     TowersOfHanoi T = new TowersOfHanoi();
     System.out.print("How many discs do you want to have(No more than 6)?  ");
     Scanner s = new Scanner(System.in);
     int d = s.nextInt();
 
     System.out.print("Initial State in tower A: ");
-    for (int j = d; j>0; j--)
-    {
+    for (int j = d; j>0; j--){
         System.out.print(j + " ");
     }
-
     System.out.println();
     T.Hanoi(d, "A", "B", "C");
 }
--- crackCodingInterview/checkPermutation.java
+++ crackCodingInterview/checkPermutation.java
@@ -1,26 +1,40 @@
 import java.util.*;
 public class checkPermutation
 {
-    public static boolean checkPerm(String one, String two)
-    {
-	char[] a = one.toCharArray();
-	char[] b = two.toCharArray();
-	Arrays.sort(a);
-	Arrays.sort(b);
-	return(Arrays.equals(a,b));
+	//helper function to get number of occurences
+    public static int Occurences(String str, char c){
+	int count = 0;
+	for(int x = 0; x < str.length();x++){
+		if(str.charAt(x) == c){
+			count++;
+		}
     }
-    public static void main(String[] args)
-    {
-	Scanner scan = new Scanner(System.in);
-	String paramOne = scan.nextLine();
-	String paramTwo = scan.nextLine();
-	if(checkPerm(paramOne,paramTwo) == true)
-	    {
-		System.out.println("The two strings are permutations of eachother");
+		return(count);
+    }
+
+    public static boolean checkPerm(String one, String two){
+		if(one.length() != two.length()){
+			return false;
+		}
+		for(int i = 0;i < one.length();i++){
+		 		if(Occurences(one,one.charAt(i)) != Occurences(two,one.charAt(i))){
+					 return(false);
+				 }
+			}
+		return(true);
+    }
+
+    public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("what is the first word that you want to use?");
+		String paramOne = scan.nextLine();
+		System.out.println("what is the second word that you want to use?");
+		String paramTwo = scan.nextLine();
+		if(checkPerm(paramOne,paramTwo) == true){
+			System.out.println("The two strings are permutations of eachother");
 	    }
-	else
-	    {
-		System.out.println("The two strings are not permutations of eachother");
+		else{
+			System.out.println("The two strings are not permutations of eachother");
 	    }
     }
 }
