93cf7f455848d4de8631b3c7baa66cea72362548
Shreyas Ramanujam <Ramanujamshreyas4@gmail.com>
2019-01-15 22:01:20 -0800

    Adding changes to BinaryTree

--- crackCodingInterview/chapter_1/BinaryTree.java
+++ crackCodingInterview/chapter_1/BinaryTree.java
@@ -44,11 +44,74 @@ public class BinaryTree {
         curr = queue.peek();
         }
     }
+
+    public void search(int val){
+        int checks = 0;
+        Queue<Node> queue = new LinkedList<Node>();
+        curr = root;
+        queue.add(curr);
+        while(!queue.isEmpty()){
+            if(curr.val == val) {
+                System.out.println("Found " + val + " in " + checks + " turns");
+                checks++;
+                break;
+            }
+            else if(curr.val == val){
+                System.out.println("Found " + val + " in " + checks + " turns");
+                checks++;
+                break;
+            }
+            else{
+            if(curr.left != null) {
+                queue.add(curr.left);
+            }
+            if(curr.right != null){
+                queue.add(curr.right);
+            }
+        }
+            checks++;
+            queue.remove();
+            curr = queue.peek();
+        }
+        if(queue.isEmpty()){
+            System.out.println(val + " doesnt exist in the tree");
+        }
+    }
+
+    public void MinValue(){
+        int min = root.val;
+        Queue<Node> queue = new LinkedList<Node>();
+        if(root.left != null){
+            queue.add(root.left);
+        }
+        if(root.right != null){
+            queue.add(root.right);
+        }
+        while(!queue.isEmpty()){
+            curr = queue.peek();
+            queue.remove();
+            if(curr.val < min){
+                min = curr.val;
+            }
+        }
+        System.out.println("The minimum value in the tree is " + min);
+    }
     public static void main(String[] args){
         BinaryTree tree = new BinaryTree(1);
         tree.add(new Node(10));
         tree.add(new Node(14));
         tree.add(new Node(16));
+        tree.add(new Node(13));
+        tree.add(new Node(63));
+        tree.add(new Node(42));
+        tree.add(new Node(10));
+        tree.add(new Node(17));
+        tree.add(new Node(18));
+        tree.add(new Node(32));
+        tree.add(new Node(52));
+        tree.add(new Node(24));
+        // tree.search(80);
+        tree.MinValue();
 
     }
 }
